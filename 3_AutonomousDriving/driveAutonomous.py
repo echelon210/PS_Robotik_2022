2691e2b756b2527d821acc0d2a4256608b562016
echelon210 <unknown_user_3721@protonmail.com>
2022-07-06 00:14:31 +0200

    Included bufferless video capture

--- 3_AutonomousDriving/driveAutonomous.py
+++ 3_AutonomousDriving/driveAutonomous.py
@@ -1,12 +1,12 @@
-import torchvision
-import torch
-import torchvision.transforms as transforms
-import torch.nn.functional as F
 import cv2
-from camera_pipeline import *
-import time
 import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 from jetbot import *
+
+from camera_pipeline import *
 
 # CREATE DASHBOARD
 cv2.namedWindow("Jetbot Dashboard")
@@ -65,9 +65,39 @@ def calculate_controller(angle, angle_last):
     return motor_left, motor_right, angle, speed
 
 
+import cv2, queue, threading, time
+
+
+# bufferless VideoCapture
+class VideoCapture:
+
+    def __init__(self, name):
+        self.cap = cv2.VideoCapture(name)
+        self.q = queue.Queue()
+        t = threading.Thread(target=self._reader)
+        t.daemon = True
+        t.start()
+
+    # read frames as soon as they are available, keeping only most recent one
+    def _reader(self):
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            if not self.q.empty():
+                try:
+                    self.q.get_nowait()  # discard previous (unprocessed) frame
+                except queue.Empty:
+                    pass
+            self.q.put(frame)
+
+    def read(self):
+        return self.q.get()
+
+
 def drive(robot):
     # Open video stream
-    video_capture = cv2.VideoCapture(jetbot_camera_pipline(), cv2.CAP_GSTREAMER)
+    video_capture = VideoCapture(jetbot_camera_pipline(), cv2.CAP_GSTREAMER)
 
     angle = 0.0
     angle_last = 0.0
